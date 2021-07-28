84da51a1848d87ed6877274c8a22361009f6e14e
Stephen Newman <steve@newman.digital>
2021-07-28 11:32:40 +0100

    Add a second test.

--- test/load/Pokedex.Api.LoadTest/PokemonTests.cs
+++ test/load/Pokedex.Api.LoadTest/PokemonTests.cs
@@ -10,7 +10,7 @@ namespace Pokedex.Api.LoadTest
     {
         [Fact]
         [Trait("Category", "LoadTest")]
-        public void Translations_Are_Quick_Enough()
+        public void Retrievals_Are_Quick_Enough()
         {
             // Arrange
             var step = Step.Create(
@@ -36,6 +36,37 @@ namespace Pokedex.Api.LoadTest
             // Assert
             stats.ScenarioStats[0].StepStats[0].Fail.Request.Count.Should().Be(0);
             stats.ScenarioStats[0].StepStats[0].Ok.Latency.Percent95.Should().BeLessOrEqualTo(750);
+        }
+        
+        [Fact]
+        [Trait("Category", "LoadTest")]
+        public void Translations_Are_Quick_Enough()
+        {
+            // Arrange
+            var step = Step.Create(
+                "fetch_squirtle_information",
+                HttpClientFactory.Create(),
+                context =>
+                {
+                    var request = Http.CreateRequest("GET", "http://localhost:5000/pokemon/translated/squirtle");
+
+                    return Http.Send(request, context);
+                },
+                timeout: TimeSpan.FromSeconds(2));
+
+            var scenario = ScenarioBuilder
+                .CreateScenario("fetch_squirtle_information", step)
+                .WithWarmUpDuration(TimeSpan.FromSeconds(10))
+                .WithLoadSimulations(
+                    Simulation.RampConstant(100, TimeSpan.FromSeconds(30)),
+                    Simulation.RampPerSec(5, TimeSpan.FromSeconds(30)));
+
+            // Act
+            var stats = NBomberRunner.RegisterScenarios(scenario).Run();
+
+            // Assert
+            stats.ScenarioStats[0].StepStats[0].Fail.Request.Count.Should().Be(0);
+            stats.ScenarioStats[0].StepStats[0].Ok.Latency.Percent95.Should().BeLessOrEqualTo(1500);
         }
     }
 }
